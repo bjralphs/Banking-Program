c3a077a7fc0c42d16c08a8e5ed4d05f11c52680a
Bryan Ralphs <38300718+bjralphs@users.noreply.github.com>
2022-08-26 13:02:18 -0600

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,124 @@
+﻿/*
+I declare that the following source code was written solely by me. 
+I understand that copying any source code, in whole or in part, constitutes cheating, 
+and that I will receive a zero on this project if I am found in violation of this policy.
+*/
+using System;
+
+public class Account
+{
+    //Declaration of variables.
+    public static string name;
+    public static string address;
+    public static int accountNumber = 12345678;
+    public static decimal balance;
+    public const int minimum = 100;
+    public static int state;
+
+    //Accoutn State Enum
+    public enum AccountState
+    {
+        New,        // 0
+        Active,     // 1
+        UnderAudit, // 2
+        Frozen,     // 3
+        Closed      // 4
+    }
+    //Name Method Getter & Setter
+    public bool SetName(string inName)
+    {
+        if (inName is null or "")
+        {
+            return false;
+        }
+        else
+        {
+            name = inName;
+            return true;
+        }
+    }
+
+    public static string GetName()
+    {
+        if (name is null or "")
+        {
+            return "No name in system.";
+        }
+        else
+        {
+            return name;
+        }
+    }
+    //Address Method Getter & Setter
+    public bool SetAddress(string inAddress)
+    {
+        if (inAddress is null or "")
+        {
+            return false;
+        }
+        else
+        {
+            address = inAddress;
+            return true;
+        }
+    }
+    public static string GetAddress()
+    {
+        if (address is null or "")
+        {
+            return "No address in system.";
+        }
+        else
+        {
+            return address;
+        }
+    }
+    //Deposit & Withdrawal methods
+    public void PayInFunds(decimal amount)
+    {
+        balance = balance + amount;
+    }
+    public bool WithdrawFunds(decimal amount)
+    {
+        balance = balance - amount;
+        if (balance < 0)
+        {
+            balance = balance + amount;
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    //Balance Method Getter & Setter
+    public bool SetBalance(decimal inBalance)
+    {
+        if (inBalance < minimum)
+        {
+            return false;
+        }
+        else
+        {
+            balance = inBalance;
+            return true;
+        }
+    }
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+    //State Method Getter & Setter
+    public void SetState(int num)
+    {
+        state = 0;
+    }
+
+    public int GetState()
+    {
+        return state;
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+I declare that the following source code was written solely by me. 
+I understand that copying any source code, in whole or in part, constitutes cheating, 
+and that I will receive a zero on this project if I am found in violation of this policy.
+*/
+using System;
+
+public class IAccount
+{
+    static public void Main(String[] args)
+    {
+        Console.WriteLine("Welcome to the UVU Banking Program");
+        Account account = new Account(); //instantiate account class
+
+        //Name method
+        account.SetName("Jimmy Johns");
+        string x = Account.GetName();
+        Console.WriteLine("Name: " + x);
+
+        //Address method
+        account.SetAddress("123 N Nunya St.");
+        string y = Account.GetAddress();
+        Console.WriteLine("Address: " + y);
+
+        //Account Number
+        Console.WriteLine("account Number: "
+            + Account.accountNumber);
+
+        //Print Begining Balance
+        Console.WriteLine("Balance: " + Account.balance);
+
+        //State method
+        account.SetState(0);
+        int v = account.GetState();
+        Account.AccountState myVar = (Account.AccountState)(v);
+        Console.WriteLine("Account State: " + myVar);
+
+        //test Deposit
+        Console.WriteLine("***Depositing 200.00***");
+        account.PayInFunds((decimal)200.00);
+        decimal w = account.GetBalance();
+        Console.WriteLine("New Balance: " + w);
+
+        //test withdrawal
+        Console.WriteLine("***Withdrawing 50.00***");
+        account.WithdrawFunds((decimal)50.00);
+        decimal z = account.GetBalance();
+        Console.WriteLine("New Balance: " + z);
+
+        //test overdrawn
+        Console.WriteLine("***Test withdraw of 500.00***");
+        if (account.WithdrawFunds((decimal)500.00) is false)
+        {
+            Console.WriteLine("Not enough funds to withdraw");
+        }
+        else
+        {
+            decimal f = account.GetBalance();
+            Console.WriteLine("New Balance: " + f);
+        }
+    }
+}
+
+
